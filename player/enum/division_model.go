59ddee6998d4763583ab3db574fef6849e99d25d
HikariYlan <hikariylandev@gmail.com>
2026-07-14 17:02:09 +0200

    feat: implemented Player division

---
+++ player/enum/division_model.go
@@ -0,0 +1,6 @@
+package enum
+
+// ENUM(beginnerOne, beginnerTwo, beginnerThree, advancedOne, advancedTwo, advancedThree, professionalOne, professionalTwo, professionalThree)
+type Division int
+
+//go tool go-enum -f division_model.go
