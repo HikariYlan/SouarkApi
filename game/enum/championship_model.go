b181353a9c2daef4d85f76a3776ca8d712ba09b1
HikariYlan <hikariylandev@gmail.com>
2026-07-19 22:45:42 +0200

    feat: implemented Championship enum

---
+++ game/enum/championship_model.go
@@ -0,0 +1,6 @@
+package enum
+
+// ENUM(talcCup, haliteCup, fluoriteCup, quartzChampionship, pyriteChamionship, garnetChampionship, corundumMasters, diamondApex, bismuthGrandChampionship)
+type Championship int
+
+//go tool go-enum -f championship_model.go
